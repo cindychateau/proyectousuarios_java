ce66f7443745bddd8dc953fbaf30943a1437db3b
Cynthia Castillo <ca.castillo@gmail.com>
2022-12-19 17:00:44 -0600

    Formularios y sesion

--- src/main/java/com/codingdojo/cynthia/controladores/ControladorUsuarioPlantillas.java
+++ src/main/java/com/codingdojo/cynthia/controladores/ControladorUsuarioPlantillas.java
@@ -2,9 +2,14 @@ package com.codingdojo.cynthia.controladores;
 
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ControladorUsuarioPlantillas {
@@ -24,6 +29,31 @@ public class ControladorUsuarioPlantillas {
 		model.addAttribute("usuarios", usuarios);
 		
 		return "index.jsp";
+	}
+	
+	@GetMapping("/registro")
+	//@RequestMapping(value="/registro", method=RequestMethod.GET)
+	public String registro() {
+		return "registro.jsp";
+	}
+	
+	@RequestMapping(value="/registrar", method=RequestMethod.POST)
+	public String registrar(@RequestParam(value="nombre") String name,
+							@RequestParam(value="email") String email,
+							HttpSession session /*Se agrega cuando queremos guardar información en sesión*/){
+		
+		System.out.println(name);
+		System.out.println(email);
+		
+		session.setAttribute("nombre", name);
+		
+		return "redirect:/dashboard";
+		
+	}
+	
+	@GetMapping("/dashboard")
+	public String dashboard() {
+		return "dashboard.jsp";
 	}
 	
 }
---
+++ src/main/webapp/WEB-INF/registro.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Registro de Usuario</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		<h1>Crear Usuario</h1>
+		<form action="/registrar" method="post">
+			<div>
+				<label>Nombre:</label>
+				<input type="text" name="nombre">
+			</div>
+			<div>
+				<label>Email:</label>
+				<input type="email" name="email">
+			</div>
+			<input type="submit" value="Guardar" class="btn btn-success" >
+		</form>
+	</div>
+</body>
+</html>
