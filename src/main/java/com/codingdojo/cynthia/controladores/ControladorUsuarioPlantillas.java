926c9a0cb1448b86a34073d90d50312f919bc788
Cynthia Castillo <ca.castillo@gmail.com>
2022-12-22 18:18:26 -0600

    Index, Nuevo, Borrar

--- src/main/java/com/codingdojo/cynthia/controladores/ControladorUsuarioPlantillas.java
+++ src/main/java/com/codingdojo/cynthia/controladores/ControladorUsuarioPlantillas.java
@@ -1,19 +1,36 @@
 package com.codingdojo.cynthia.controladores;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.codingdojo.cynthia.modelos.Usuario;
+import com.codingdojo.cynthia.servicios.Servicios;
+
 @Controller
 public class ControladorUsuarioPlantillas {
+	
+	private final Servicios servicio;
+	
+	public ControladorUsuarioPlantillas(Servicios servicio) {
+		this.servicio = servicio;
+	}
+	
 	
 	@GetMapping("/")
 	public String index(Model model) {
@@ -64,8 +81,40 @@ public class ControladorUsuarioPlantillas {
 	}
 	
 	@GetMapping("/dashboard")
-	public String dashboard() {
+	public String dashboard(Model model) {
+		
+		List <Usuario> usuarios = servicio.find_usuarios();
+		
+		//Creo variable usuarios que tiene el valor de la lista con objetos de usuario
+		model.addAttribute("usuarios", usuarios); 
+ 		
 		return "dashboard.jsp";
+	}
+	
+	@GetMapping("/new")
+	public String new_user(@ModelAttribute("usuario") Usuario usuario) {
+		//En base a lo que el usuario ingrese, va a crear un objeto de usuario
+		return "new.jsp";
+	}
+	
+	//@RequestMapping(value="/create", method=RequestMethod.POST)
+	@PostMapping("/create")
+	public String create(@Valid @ModelAttribute("usuario") Usuario usuario,
+						 BindingResult result /*Encargado de regresar los mensajes de valid*/ ) {
+		
+		if (result.hasErrors()) {
+			return "new.jsp";
+		} else {
+			servicio.save_usuario(usuario);
+			return "redirect:/dashboard";
+		}
+		
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String delete_user(@PathVariable("id") Long id) {
+		servicio.delete_usuario(id);
+		return "redirect:/dashboard";
 	}
 	
 }
--- src/main/webapp/WEB-INF/dashboard.jsp
+++ src/main/webapp/WEB-INF/dashboard.jsp
@@ -11,6 +11,32 @@
 <body>
 	<div class="container">
 		<h1>¡Bienvenid@ <c:out value="${nombre}" /> !</h1>
+		<table class="table table-hover">
+			<thead>
+				<tr>
+					<th>Nombre</th>
+					<th>Apellido</th>
+					<th>E-mail</th>
+					<th>Acciones</th>
+				</tr>
+			</thead>
+			<tbody>
+				<c:forEach items="${usuarios}" var="usuario">
+					<tr>
+						<td> <c:out value="${usuario.getFirst_name()}" /> </td>
+						<td> <c:out value="${usuario.getLast_name()}" /> </td>
+						<td> <c:out value="${usuario.getEmail()}" /> </td>
+						<td>
+							<!--  PENDIENTE editar buttons -->
+							<form action="/delete/${usuario.getId()}" method="post">
+								<input type="hidden" name="_method" value="DELETE" />
+								<input type="submit" value="Eliminar" class="btn btn-danger" />
+							</form>
+						</td>
+					</tr>
+				</c:forEach>
+			</tbody>
+		</table>
 	</div>
 </body>
 </html>
